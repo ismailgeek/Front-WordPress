f02317bc63d39327e4ec0e7bb2ccc86a7dd67d1e
ismailbourhim <ismailbourhim9@gmail.com>
2023-10-23 00:33:26 +0100

    function php

--- wp-content/themes/fontainebleau-child/functions.php
+++ wp-content/themes/fontainebleau-child/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * fontainebleau child Theme functions and definitions
  *
@@ -11,38 +12,43 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_FONTAINEBLEAU_CHILD_VERSION', '1.0.0' );
+define('CHILD_THEME_FONTAINEBLEAU_CHILD_VERSION', '1.0.0');
 
 /**
  * Enqueue styles
  */
-function child_enqueue_styles() {
+function child_enqueue_styles()
+{
 
-	wp_enqueue_style( 'fontainebleau-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_FONTAINEBLEAU_CHILD_VERSION, 'all' );
-
+	wp_enqueue_style('fontainebleau-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_FONTAINEBLEAU_CHILD_VERSION, 'all');
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_action('wp_enqueue_scripts', 'child_enqueue_styles', 15);
 
-function fixed_menu() {
-    ?>
-        <script>
-          // Function to toggle the fixed menu class
-			function toggleFixedMenu() {
-			  const menu = document.querySelector('#menu-section'); 
-				const homeMenu = document.querySelector('.home section#menu-section');
-			  if (window.scrollY > 100) {
+function fixed_menu()
+{
+?>
+	<script>
+		// Function to toggle the fixed menu class
+		function toggleFixedMenu() {
+			const menu = document.querySelector('#menu-section');
+			const homeMenu = document.querySelector('.home section#menu-section');
+			if (window.scrollY > 100) {
 				menu.classList.add('fixed-menu');
-				  homeMenu.style.display = 'block';
-			  } else {
+				if (homeMenu) {
+					homeMenu.style.display = 'block';
+				}
+			} else {
 				menu.classList.remove('fixed-menu');
-				  homeMenu.style.display = 'none';
-			  }
+				if (homeMenu) {
+					homeMenu.style.display = 'none';
+				}
 			}
+		}
 
-			// Attach the scroll event listener to the window
-			window.addEventListener('scroll', toggleFixedMenu);
-        </script>
-    <?php
+		// Attach the scroll event listener to the window
+		window.addEventListener('scroll', toggleFixedMenu);
+	</script>
+<?php
 }
 add_action('wp_head', 'fixed_menu');
